2a2829243d389bb866f7fbf30e35b3819af189f5
Elias Ramos <elias_@Live.jp>
2021-09-27 17:31:58 +0100

    limpeza

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 
-import Scrapper
+import scrapper
 
-#DataSet
-import pandas as pd
 
-user="esr-music8"
-user_followers = Scrapper.get_user_followers(user)
-print(user_followers)
+# DataSet
+
+user = "esr-music8"
+
+user_followers = scrapper.get_user_followers(user)
+
+user_tracks = scrapper.get_user_track_list(user)
+
+
+print(user_tracks)
