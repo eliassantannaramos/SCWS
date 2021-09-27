8c5cc269dcfdc3389283b6201863f3abda5f3d48
Elias Ramos <elias_@Live.jp>
2021-09-27 21:34:45 +0100

    Script completo

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 import scrapper
 
-
 # DataSet
+import pandas as pd
 
 user = "esr-music8"
 
@@ -10,5 +10,16 @@ user_followers = scrapper.get_user_followers(user)
 
 user_tracks = scrapper.get_user_track_list(user)
 
+db_track_likes = db_track_repost = db_track_comments = {}
+for track in user_tracks:  # Get all user the likes all your tracks
+    db_track_likes[track] = scrapper.get_user_track_likes(track)
+    db_track_repost[track] = scrapper.get_track_repost(track)
+    db_track_comments[track] = scrapper.get_track_comments(track)
 
-print(user_tracks)
+pd.DataFrame(list(user_tracks.items()),
+             columns=["Link", "track_name"]).to_csv("user_tracks.csv", index=False)
+pd.DataFrame(list(user_followers.items()),
+             columns=["Follower_Link", "Follower"]).to_csv("user_followers.csv", index=False)
+pd.DataFrame(db_track_likes).to_csv("db_track_likes.csv", index=True)
+pd.DataFrame(db_track_repost).to_csv("db_track_repost.csv", index=True)
+pd.DataFrame(db_track_comments).to_csv("db_track_comments.csv", index=True)
