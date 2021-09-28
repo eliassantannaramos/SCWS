23db43805cd165560b1af2f6d1753a1ebefeb11e
Lucas Paiva <lucas3f@gmail.com>
2021-09-28 16:19:06 -0300

    first env setup. requirements added.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 
 import scrapper
-
 # DataSet
-import pandas as pd
-
 user = "esr-music8"
 
 user_followers = scrapper.get_user_followers(user)
@@ -16,10 +13,11 @@ for track in user_tracks:  # Get all user the likes all your tracks
     db_track_repost[track] = scrapper.get_track_repost(track)
     db_track_comments[track] = scrapper.get_track_comments(track)
 
-pd.DataFrame(list(user_tracks.items()),
-             columns=["Link", "track_name"]).to_csv("user_tracks.csv", index=False)
-pd.DataFrame(list(user_followers.items()),
-             columns=["Follower_Link", "Follower"]).to_csv("user_followers.csv", index=False)
-pd.DataFrame(db_track_likes).to_csv("db_track_likes.csv", index=True)
-pd.DataFrame(db_track_repost).to_csv("db_track_repost.csv", index=True)
-pd.DataFrame(db_track_comments).to_csv("db_track_comments.csv", index=True)
+print(db_track_likes)
+# pd.DataFrame(list(user_tracks.items()),
+#              columns=["Link", "track_name"]).to_csv("user_tracks.csv", index=False)
+# pd.DataFrame(list(user_followers.items()),
+#              columns=["Follower_Link", "Follower"]).to_csv("user_followers.csv", index=False)
+# pd.DataFrame(db_track_likes).to_csv("db_track_likes.csv", index=True)
+# pd.DataFrame(db_track_repost).to_csv("db_track_repost.csv", index=True)
+# pd.DataFrame(db_track_comments).to_csv("db_track_comments.csv", index=True)
